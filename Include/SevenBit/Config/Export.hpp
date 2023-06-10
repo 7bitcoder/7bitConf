671b49c214f608194e69453d03592a6abfa130a5
7bitcoder <Sylwo1003@gmail.com>
2023-06-10 13:59:14 +0200

    rename lib to 7bitConf

--- Include/SevenBit/Config/Export.hpp
+++ Include/SevenBit/Config/Export.hpp
@@ -7,7 +7,7 @@
 
 #if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
 //  Microsoft
-#ifdef _7bitConfig_EXPORTS
+#ifdef _7bitConf_EXPORTS
 /* We are building this library */
 #define EXPORT __declspec(dllexport)
 #else
@@ -17,7 +17,7 @@
 
 #elif defined(__linux__) || defined(UNIX) || defined(__unix__) || defined(LINUX)
 //  GCC
-#if defined(_7bitConfig_EXPORTS)
+#if defined(_7bitConf_EXPORTS)
 #define EXPORT __attribute__((visibility("default")))
 #else
 #define EXPORT
