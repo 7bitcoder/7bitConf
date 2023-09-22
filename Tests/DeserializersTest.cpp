d428181f82e91da00e72b4a7624a2e2ef965b73a
7bitcoder <Sylwo1003@gmail.com>
2023-09-22 21:50:58 +0200

    update tests for setting parser

--- Tests/DeserializersTest.cpp
+++ Tests/DeserializersTest.cpp
@@ -1,104 +1,82 @@
-// #include <gtest/gtest-param-test.h>
-// #include <gtest/gtest.h>
-// #include <gtest/internal/gtest-param-util.h>
-// #include <random>
-// #include <string_view>
-// #include <tuple>
-//
-// #include "SevenBit/Conf/Details/Utils.hpp"
-// #include "SevenBit/Conf/Details/ValueDeserializers.hpp"
-// #include "SevenBit/Conf/Exceptions.hpp"
-// #include "Utilities/ParamsTest.hpp"
-//
-// class DeserializersTest : public testing::Test
-//{
-//   protected:
-//     static void TearUpTestSuite() {}
-//
-//     DeserializersTest() {}
-//
-//     void SetUp() override {}
-//
-//     void TearDown() override {}
-//
-//     ~DeserializersTest() {}
-//
-//     static void TearDownTestSuite() {}
-// };
-//
-// static Params<std::vector<std::string_view>, std::string> SettingsValues = {
-//     // String
-//     {{"option", "nested", "nested"}, "String"},
-//     {{"option"}, "StRinG"},
-//     {{"option", "nested"}, "str"},
-//     {{"option", "nested", "helloooooo", "nun"}, "str"},
-//     {{"option", "nested", "long", "nun", "long", "verryLong", "long", "long"}, "str"},
-//     {{"option", "3", "nested"}, "json"}};
-// PARAMS_TEST_COMBINED_4(SettingKeySplitterTest, ShouldSplitSettings, OptionPrefix, SettingsValues, TypeMarkers,
-//                        KeySeparators)
-//{
-//     const auto &[prefix, params, typeMark, separator] = GetParam();
-//     const auto &[key, type] = params;
-//     sb::cf::details::SettingKeySplitter splitter{OptionPrefix[1], KeySeparators[0], TypeMarkers[0], KeySeparators[1],
-//                                                  TypeMarkers[1]};
-//
-//     sb::cf::details::SettingKeySplitter::Result expected{key, type};
-//
-//     auto setting = prefix + sb::cf::details::utils::joinViews(key, separator) + typeMark + type;
-//     auto result = splitter.split(setting);
-//     EXPECT_EQ(result, expected);
-// }
-//
-// PARAMS_TEST(SettingKeySplitterTest, ShouldSplitSettingsRandom, SettingsValues)
-//{
-//     const auto &[key, type] = GetParam();
-//
-//     sb::cf::details::SettingKeySplitter splitter{OptionPrefix[1], KeySeparators[0], TypeMarkers[0], KeySeparators[1],
-//                                                  TypeMarkers[1]};
-//
-//     sb::cf::details::SettingKeySplitter::Result expected{key, type};
-//
-//     auto keys = randomJoin(key, KeySeparators);
-//     auto withOptionPrefix = randomJoin("", keys, OptionPrefix);
-//     auto setting = randomJoin(withOptionPrefix, type, TypeMarkers);
-//
-//     auto result = splitter.split(setting);
-//     EXPECT_EQ(result, expected);
-// }
-//
-// static Params<std::string_view, sb::cf::details::SettingKeySplitter::Result> ProblematicSettingsValues = {
-//     {"--option!!type", {{"option!"}, "type"}},
-//     {"--option!!", {{"option!"}, ""}},
-//     {"--option!!:inner!", {{"option!!", "inner"}, ""}},
-//     {"--option!!:inner:::!", {{"option!!", "inner", "", "", ""}, ""}},
-//     {":option:inner", {{"", "option", "inner"}, ""}},
-//     {":option!!:inner", {{"", "option!!", "inner"}, ""}},
-//     {":::!", {{"", "", "", ""}, ""}},
-//     {"::!:", {{"", "", "!", ""}, ""}},
-//     {":", {{"", ""}, ""}},
-//     {"__!", {{"", ""}, ""}},
-//     {"_______", {{"", "", ""}, ""}},
-//     {"__hello_____type", {{"", "hello", ""}, "type"}},
-//     {"__hello__type", {{"", "hello", "type"}, ""}},
-// };
-// PARAMS_TEST(SettingKeySplitterTest, ShouldSplitProblematicSettings, ProblematicSettingsValues)
-//{
-//     const auto &[setting, expected] = GetParam();
-//
-//     sb::cf::details::SettingKeySplitter splitter{OptionPrefix[1], KeySeparators[0], TypeMarkers[0], KeySeparators[1],
-//                                                  TypeMarkers[1]};
-//
-//     auto result = splitter.split(setting);
-//     EXPECT_EQ(result, expected);
-// }
-//
-// static Params<std::string_view> FailSettingsValues = {"", "--"};
-// PARAMS_TEST(SettingKeySplitterTest, ShouldFailSplitSettings, FailSettingsValues)
-//{
-//     const auto &[setting] = GetParam();
-//
-//     sb::cf::details::SettingKeySplitter splitter{OptionPrefix[1], KeySeparators[0], TypeMarkers[0], KeySeparators[1],
-//                                                  TypeMarkers[1]};
-//
-//     EXPECT_ANY_THROW(splitter.split(setting));
-// }
+#include <gtest/gtest-param-test.h>
+#include <gtest/gtest.h>
+#include <gtest/internal/gtest-param-util.h>
+#include <random>
+#include <string_view>
+#include <tuple>
+
+#include "SevenBit/Conf/Details/Deserializers.hpp"
+#include "SevenBit/Conf/Details/Utils.hpp"
+#include "SevenBit/Conf/Details/ValueDeserializersMap.hpp"
+#include "SevenBit/Conf/Exceptions.hpp"
+#include "Utilities/ParamsTest.hpp"
+
+class DeserializersTest : public testing::Test
+{
+  protected:
+    static void TearUpTestSuite() {}
+
+    DeserializersTest() {}
+
+    void SetUp() override {}
+
+    void TearDown() override {}
+
+    ~DeserializersTest() {}
+
+    static void TearDownTestSuite() {}
+};
+
+static Params<std::string_view, std::optional<std::string_view>, sb::cf::JsonValue> DeserializeData = {
+    {"int", "12", std::int64_t{12}},
+    {"int", "-12", std::int64_t{-12}},
+    {"int", std::nullopt, std::int64_t{0}},
+    {"uint", "12", std::uint64_t{12}},
+    {"uint", std::nullopt, std::uint64_t{0}},
+    {"bool", "true", true},
+    {"bool", "1", true},
+    {"bool", "-11", true},
+    {"bool", "false", false},
+    {"bool", "0", false},
+    {"bool", std::nullopt, false},
+    {"string", std::nullopt, std::string{""}},
+    {"string", "hello", std::string{"hello"}},
+    {"string", "", std::string{""}},
+    {"double", "1.1", 1.1},
+    {"double", "-11.1", -11.1},
+    {"double", std::nullopt, 0.0},
+    {"null", std::nullopt, sb::cf::json::null},
+    {"null", "hello", sb::cf::json::null},
+    {"json", "{\"json\": \"value\"}", sb::cf::JsonObject{{"json", "value"}}},
+};
+PARAMS_TEST(DeserializersTest, ShouldDeserializeValue, DeserializeData)
+{
+    const auto &[type, value, expected] = GetParam();
+    sb::cf::details::ValueDeserializersMap deserializers;
+    deserializers.add("string", std::make_unique<sb::cf::details::StringDeserializer>());
+    deserializers.add("bool", std::make_unique<sb::cf::details::BoolDeserializer>());
+    deserializers.add("int", std::make_unique<sb::cf::details::IntDeserializer>());
+    deserializers.add("double", std::make_unique<sb::cf::details::DoubleDeserializer>());
+    deserializers.add("uint", std::make_unique<sb::cf::details::UIntDeserializer>());
+    deserializers.add("json", std::make_unique<sb::cf::details::JsonDeserializer>());
+    deserializers.add("null", std::make_unique<sb::cf::details::NullDeserializer>());
+
+    auto deserializer = deserializers.getDeserializerFor(type);
+    EXPECT_TRUE(deserializer);
+    EXPECT_EQ(deserializer->deserialize(value), expected);
+}
+
+TEST_F(DeserializersTest, ShouldNotDeserializeValue)
+{
+    sb::cf::details::ValueDeserializersMap deserializers;
+    deserializers.add("string", std::make_unique<sb::cf::details::StringDeserializer>());
+    deserializers.add("bool", std::make_unique<sb::cf::details::BoolDeserializer>());
+    deserializers.add("int", std::make_unique<sb::cf::details::IntDeserializer>());
+    deserializers.add("double", std::make_unique<sb::cf::details::DoubleDeserializer>());
+    deserializers.add("uint", std::make_unique<sb::cf::details::UIntDeserializer>());
+    deserializers.add("json", std::make_unique<sb::cf::details::JsonDeserializer>());
+    deserializers.add("null", std::make_unique<sb::cf::details::NullDeserializer>());
+
+    auto deserializer = deserializers.getDeserializerFor("unknown");
+    EXPECT_FALSE(deserializer);
+}
